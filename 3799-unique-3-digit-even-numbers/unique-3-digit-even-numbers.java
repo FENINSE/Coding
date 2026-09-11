27deb6758b102424bc95b550cc09c52d8c500bc9
FENINSE <lintofeninse9@gmail.com>
2026-09-11 13:56:53 +0530

    Time: 4 ms (86.93%) | Memory: 44.6 MB (85.73%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        boolean[] vis = new boolean[1000];
+        for(int i=0;i<digits.length;i++){
+            if(digits[i]==0){
+                continue;
+            }
+            for(int j=0;j<digits.length;j++){
+                if(j==i){
+                    continue;
+                }
+                for(int k=0;k<digits.length;k++){
+                    if(k==i || k==j){
+                        continue;
+                    }
+                    int num = digits[i] * 100 + digits[j] *10 + digits[k];
+                    if(num%2==0){
+                        vis[num]=true;
+                    }
+                }
+            }
+        }
+        int count = 0;
+        for(boolean present:vis){
+            if(present){
+                count++;
+            }
+        }
+        return count;
+    }
+}
