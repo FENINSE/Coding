11f3a39121d67fe3ff34a920ac940025b9a6a135
FENINSE <lintofeninse9@gmail.com>
2026-09-20 19:53:43 +0530

    Time: 7 ms (34.31%) | Memory: 45.2 MB (51.81%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+       Arrays.sort(candidates);
+       List<List<Integer>> list = new ArrayList<>();
+       boolean[] vis = new boolean[candidates.length];
+       ArrayList<Integer> current = new ArrayList<>();
+       findcomb(candidates, target, list,current,0); 
+       return list;
+    }
+    static void findcomb(int[] arr,int target,List<List<Integer>> list,ArrayList<Integer> current,int start){
+        if(target==0){
+            list.add(new ArrayList<>(current));
+            return;
+        }
+        if(target < 0){
+            return;
+        }
+        for(int i = start;i<arr.length;i++){
+            if(i > start && arr[i]==arr[i-1]){
+                continue;
+            }
+            current.add(arr[i]);
+            findcomb(arr,target-arr[i],list,current,i+1);
+            current.remove(current.size()-1);
+        }
+    }
+}
