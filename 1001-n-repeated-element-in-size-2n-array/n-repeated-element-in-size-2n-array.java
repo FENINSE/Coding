bc67a7d5f497305166005673ca68159af76dc0f7
FENINSE <lintofeninse9@gmail.com>
2026-09-25 11:47:40 +0530

    Time: 19 ms (12.40%) | Memory: 47.2 MB (80.46%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],1);
+            }else{
+                map.put(nums[i],map.get(nums[i]) + 1);
+            }
+        }
+        int max = 0;
+        int ele = 0;
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if(value >= max){
+                max = value;
+                ele = key;
+            }
+            }
+        return ele;
+    }
+}
